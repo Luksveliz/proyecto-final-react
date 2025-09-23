0ce554c267a1dd8689ffc12f3fa261c8d8079e7d
lucasveliz <luksveliz@gmail.com>
2025-09-23 19:07:19 -0300

    estilos en el proyecto y pagina login

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -2,16 +2,18 @@ export default function Chat() {
   return (
     <div className="chat">
       <header className="chat-header">
-        <div className="chat-user">
-          <img
-            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ4YreOWfDX3kK-QLAbAL4ufCPc84ol2MA8Xg&s"
-            alt="Aiden Chavez"
-            className="chat-avatar"
-          />
-          <div>
+        <div>
+          <div className="chat-user">
+            <img
+              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ4YreOWfDX3kK-QLAbAL4ufCPc84ol2MA8Xg&s"
+              alt="Aiden Chavez"
+              className="chat-avatar"
+            />
+
             <strong>Aiden Chavez</strong>
-            <span className="last-seen"> Last seen: 2 hours ago</span>
           </div>
+
+          <span className="last-seen"> Last seen: 2 hours ago</span>
         </div>
 
         <div className="chat-actions">
@@ -33,7 +35,7 @@ export default function Chat() {
         ))}
       </section>
 
-      <footer className="chat-input">
+      <footer className="chat-footer">
         <input type="text" placeholder="Enter text here..." />
         <button>➤</button>
       </footer>
--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -35,17 +35,17 @@ const Login = () => {
   }
 
   return (
-    <main>
+    <main className="login-main">
       <img width={100} src={logo} alt="logo de whatsapp" />
       <h1>Clon de Whatsapp</h1>
       <form onSubmit={handleSubmit}>
-        <label>Contraseña de acceso</label>
-        <input type="text" onChange={(event) => setPassword(event.target.value)} />
+        <input placeholder="Ingrese la Contraseña"
+          type="text" onChange={(event) => setPassword(event.target.value)} />
         <button>Acceder</button>
         {message && <p style={{ color: "green" }}>{message}</p>}
         {error && <p style={{ color: "red" }}>{error}</p>}
       </form>
-      <p>Acceso restringido • Contenido privado</p>
+      <p className="text-info">Acceso restringido • Contenido privado</p>
     </main>
   )
 }
